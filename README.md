ae64f0d0c9ffb9457bd55f3d5ab750090a0c106a
Chris Scherm <chrisscherm@yahoo.com>
2021-06-06 12:35:07 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Test CRUD w/ MERN 
+
+## I'm using this app to learn more about MERN. In particular, React and MongoDB. 
+
+## I will add more functionality to the app as I learn more.
+
+Currently the app allows me to create a simple list of friends similarly to the contacts section on your phone.
+
+Planned:
+- Add the ability to post and delete messages
+- Logins and multiple users
+- Ability to see other user posts
+
+
+
---
+++ server/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const cors = require('cors'); 
+//when working with react front end and node back end you need cors
+const mongoose = require('mongoose');
+const FriendModel = require('./models/Friends');
+
+  
+
+//need midware to accept json
+app.use(cors());
+app.use(express.json());
+
+
+
+/// DATABASE CONNECTION
+mongoose.connect(
+    "mongodb://localhost:27017/tutorialmern?readPreference=primary&appname=MongoDB%20Compass&ssl=false", 
+    {useNewUrlParser: true }
+);
+
+
+
+
+app.post("/addfriend", async (req, res) => {
+    
+    const name = req.body.name;
+    const age = req.body.age;
+
+     const friend = new FriendModel({ name: name, age: age });
+     await friend.save();
+     res.send(friend);
+});
+
+app.get("/read", async (req, res) => {
+    FriendModel.find({}, (err, result) => { 
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+
+
+app.put("/update", async (req, res) => {
+    const newAge = req.body.newAge;
+    const id = req.body.id;
+
+    try{
+        await FriendModel.findById(id, (error, friendToUpdate) => {
+            friendToUpdate.age = Number(newAge);
+            friendToUpdate.save();
+        });
+    }   catch(err) {
+        console.log(err);
+    }
+
+    res.send("updated");
+});
+
+
+app.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    await FriendModel.findByIdAndRemove(id).exec();
+    res.send("item deleted!");
+});
+
+app.listen(3001, () => {
+    console.log("You are connected!");
+});
+
